58d4e45e09532f853e5ba20913bfea1eaa5b41ae
cbarreramartin0 <krito1305@hotmaail.com>
2021-06-01 19:31:55 -0700

    change color to blue and bigger size

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View } from "react-native";
 import styles from "./my-style";
 
 export default function App() {
